340f8b1ef3bad8f9f5f63727d2821656205fb832
Jakub Sumpor <sj51112@zut.edu.pl>
2024-10-21 01:19:25 +0200

    Sync, sync checkbox and ui fixes
    Sync function need fixing

--- src/ui.rs
+++ src/ui.rs
@@ -2,20 +2,15 @@ use std::env;
 use crate::wav::compute_signal;
 use gtk4::prelude::*;
 use gtk4::{gdk, glib};
-use gtk4::prelude::WidgetExt;
-
-pub struct GLOBALS {
-    pub debug: bool,
-}
+use std::cell::Cell;
+use std::rc::Rc;
 
 fn load_css() {
-    // Get the color scheme from the settings
     let settings = gio::Settings::new("org.gnome.desktop.interface");
     let color_scheme = settings.string("color-scheme");
     let theme = settings.string("gtk-theme");
     let is_dark_theme = color_scheme.eq("prefer-dark");
 
-    // Get the path to the CSS file
     let theme_path = if !is_dark_theme {
         format!("/usr/share/themes/{}/gtk-4.0/gtk.css", theme)
     } else {
@@ -25,11 +20,9 @@ fn load_css() {
         )
     };
 
-    // Load the CSS file and add it to the provider
     let provider = gtk4::CssProvider::new();
     provider.load_from_path(theme_path);
 
-    // Add the provider to the default screen
     gtk4::style_context_add_provider_for_display(
         &gdk::Display::default().expect("Could not connect to a display."),
         &provider,
@@ -40,28 +33,17 @@ fn load_css() {
 pub fn build_ui(app: &gtk4::Application) {
     load_css();
 
-    // Get the debug flag from the environment
-    let mut debug: bool = false;
-    let key: &str = "DEBUG";
-    if env::var_os(key).is_some() {
-        if let Ok(val) = env::var(key) {
-            debug = val == "1";
-        }
-    }
-    let globals: GLOBALS = GLOBALS { debug };
+    let debug: bool = env::var("DEBUG").map_or(false, |v| v == "1");
+    let sync = Rc::new(Cell::new(false));
 
-    // Text box with path
     let text_box = gtk4::Entry::new();
     text_box.set_placeholder_text(Some("Select a WAV file..."));
 
-    // Create button "Proceed"
-    let button_proceed: gtk4::Button = gtk4::Button::with_label("Proceed");
+    let button_proceed = gtk4::Button::with_label("Proceed");
     button_proceed.set_sensitive(false);
 
-    // Create a button
     let button_open_file = gtk4::Button::with_label("Open File");
     button_open_file.connect_clicked(glib::clone!(@weak app, @weak text_box, @weak button_proceed => move |_| {
-        // Create a file chooser dialog
         let dialog = gtk4::FileChooserDialog::new(
             Some("Select a WAV file"),
             Some(&app.active_window().unwrap()),
@@ -73,13 +55,12 @@ pub fn build_ui(app: &gtk4::Application) {
         filter.set_name(Some("WAV files"));
         filter.add_mime_type("audio/x-wav");
         dialog.add_filter(&filter);
-        dialog.connect_response(glib::clone!(@weak app => move |dialog, response| {
+        dialog.connect_response(glib::clone!(@weak app, @weak text_box, @weak button_proceed => move |dialog, response| {
             if response == gtk4::ResponseType::Ok {
-                // Open the selected file
                 if let Some(file) = dialog.file() {
                     if let Some(path) = file.path() {
                         text_box.set_text(path.to_str().unwrap());
-                        button_proceed.set_sensitive(true);
+                        button_proceed.set_sensitive(true); // Upewnij się, że przycisk jest włączony po wyborze pliku
                     }
                 }
             }
@@ -88,13 +69,19 @@ pub fn build_ui(app: &gtk4::Application) {
         dialog.show();
     }));
 
-    // Main vertical box to organize layout
+    let checkbox_sync = gtk4::CheckButton::with_label("Sync");
+    checkbox_sync.set_active(false);
+
+    let sync_clone = Rc::clone(&sync);
+    checkbox_sync.connect_toggled(move |checkbox_sync| {
+        println!("Sync: {}", checkbox_sync.is_active());
+        sync_clone.set(checkbox_sync.is_active());
+    });
+    
     let main_vbox = gtk4::Box::new(gtk4::Orientation::Vertical, 12);
-    // Make the window, grid, and all child widgets expand fully
     main_vbox.set_hexpand(true);
     main_vbox.set_vexpand(true);
 
-    // Grid layout for the top part (text_box and buttons)
     let top_grid = gtk4::Grid::new();
     top_grid.set_column_spacing(12);
     top_grid.set_row_spacing(12);
@@ -103,71 +90,63 @@ pub fn build_ui(app: &gtk4::Application) {
     top_grid.set_margin_start(12);
     top_grid.set_margin_end(12);
 
-    // Add text_box to the first column, spanning most of the width
     top_grid.attach(&text_box, 0, 0, 1, 1);
-    text_box.set_hexpand(true); // Expand to full available width
+    text_box.set_hexpand(true);
 
-    // Add button next to the text_box
     top_grid.attach(&button_open_file, 1, 0, 1, 1);
-    button_open_file.set_hexpand(false); // Do not expand as much as text_box
+    button_open_file.set_hexpand(false);
 
-    // Add button_proceed below button
     top_grid.attach(&button_proceed, 1, 1, 1, 1);
+    top_grid.attach(&checkbox_sync, 0, 1, 1, 1);
 
-    // Add the top_grid to the main_vbox
     main_vbox.append(&top_grid);
 
-    // Create a box for the image (bottom section)
     let image_box = gtk4::Box::new(gtk4::Orientation::Vertical, 12);
     image_box.set_hexpand(true);
     image_box.set_vexpand(true);
-
-    // Add image_box to the main_vbox (bottom section)
     main_vbox.append(&image_box);
 
-    // Connect the button to the compute signal function
-    button_proceed.connect_clicked(glib::clone!(@weak app, @weak text_box, @weak image_box => move |_| {
-        let mut path: String = String::new();
+    // Po kliknięciu przycisku "Proceed" wywołaj compute_signal z globals
+    let sync_clone = Rc::clone(&sync);
+    button_proceed.connect_clicked(glib::clone!(@weak text_box, @weak image_box, @strong debug, @weak sync_clone => move |_| {
         let filename = text_box.text();
         if !filename.is_empty() {
-            path = compute_signal(filename.as_str(), &globals);
+            let path = compute_signal(&filename, &debug, &sync_clone.get());
+            if !path.is_empty() {
+                // Clear the image_box by removing all children
+                if let Some(mut child) = image_box.first_child() {
+                    while let Some(next) = child.next_sibling() {
+                        image_box.remove(&child);
+                        child = next;
+                    }
+                    // Remove the last remaining child
+                    image_box.remove(&child);
+                }
+    
+                let img = gtk4::Image::new();
+                img.set_from_file(Some(path));
+                img.set_hexpand(true);
+                img.set_vexpand(true);
+                img.set_halign(gtk4::Align::Fill);
+                img.set_valign(gtk4::Align::Fill);
+    
+                image_box.set_hexpand(true);
+                image_box.set_vexpand(true);
+                image_box.set_halign(gtk4::Align::Fill);
+                image_box.set_valign(gtk4::Align::Fill);
+    
+                image_box.append(&img);
+                image_box.show();
+            }
         }
-        if !path.is_empty() {
-            // **************
-            // TODO: Fix image displaying size to fit screen
-            // **************
+    }));
 
-            // Create a new image and add it to the image_box
-            let img = gtk4::Image::new();
-            img.set_from_file(Some(path));
-    
-            // Make the image expand to the full available space
-            img.set_hexpand(true);
-            img.set_vexpand(true);
-            img.set_halign(gtk4::Align::Fill);
-            img.set_valign(gtk4::Align::Fill);
-    
-            // Make sure the image box also expands
-            image_box.set_hexpand(true);
-            image_box.set_vexpand(true);
-            image_box.set_halign(gtk4::Align::Fill);
-            image_box.set_valign(gtk4::Align::Fill);
-    
-            image_box.append(&img);
-    
-            // Show the updated layout
-            image_box.show();
-        }
-    }));    
-
-    // Create a window and add the main_vbox as the child
     let window = gtk4::ApplicationWindow::builder()
         .application(app)
         .title("trans-misja")
         .child(&main_vbox)
         .build();
 
-    window.set_default_size(800, 600); // Optional: Set a default size for the window
-
+    window.set_default_size(800, 600);
     window.present();
 }
--- src/wav.rs
+++ src/wav.rs
@@ -1,15 +1,15 @@
-use crate::ui::GLOBALS;
 use hound::WavReader;
 use image::{GrayImage, ImageBuffer, Luma};
-use plotly::{Plot, Scatter};
 
-pub fn compute_signal(filepath: &str, globals: &GLOBALS) -> String {
+pub fn compute_signal(filepath: &str, debug: &bool, sync: &bool) -> String {
+    println!("Debug: {}, Sync: {}", debug, sync);
+
     /*
         Loading wav files with hound
     */
     let mut reader = WavReader::open(filepath).unwrap();
     let spec = reader.spec();
-    if globals.debug {
+    if *debug {
         println!("Wav file: {}", filepath);
         println!("Sample rate: {}", spec.sample_rate);
         println!("Channels: {}", spec.channels);
@@ -39,7 +39,10 @@ pub fn compute_signal(filepath: &str, globals: &GLOBALS) -> String {
 
     let mut samples: Vec<f32> = Vec::new();
     if spec.sample_format == hound::SampleFormat::Float {
-        let samples_float = reader.samples::<f32>().collect::<Result<Vec<f32>, hound::Error>>().unwrap();
+        let samples_float = reader
+            .samples::<f32>()
+            .collect::<Result<Vec<f32>, hound::Error>>()
+            .unwrap();
         let channels = spec.channels as usize;
         let mut i = 0;
         while i < samples_float.len() {
@@ -49,7 +52,10 @@ pub fn compute_signal(filepath: &str, globals: &GLOBALS) -> String {
             i += 1;
         }
     } else if spec.sample_format == hound::SampleFormat::Int {
-        let samples_int = reader.samples::<i16>().collect::<Result<Vec<i16>, hound::Error>>().unwrap();
+        let samples_int = reader
+            .samples::<i16>()
+            .collect::<Result<Vec<i16>, hound::Error>>()
+            .unwrap();
         let channels = spec.channels as usize;
         let mut i = 0;
         while i < samples_int.len() {
@@ -66,17 +72,9 @@ pub fn compute_signal(filepath: &str, globals: &GLOBALS) -> String {
     }
     println!("(...)");
 
+    // Resampling
     let ratio = target_sample_rate as f64 / spec.sample_rate as f64;
-
-    let mut resampled_samples: Vec<f32> = Vec::new();
-    for i in 0..(samples.len() as f64 * ratio) as usize {
-        let index = (i as f64 / ratio) as usize;
-        if index < samples.len() {
-            let x = (i as f64 / ratio) as f64 - index as f64;
-            let y: f32 = samples[index] as f32 + x as f32 * ((samples[index + 1] as f32) - (samples[index] as f32));
-            resampled_samples.push(y);
-        }
-    }
+    let resampled_samples = resample_signal(samples, ratio);
 
     println!("Resampled samples: {}", resampled_samples.len());
     for sample in resampled_samples.iter().take(100) {
@@ -85,9 +83,19 @@ pub fn compute_signal(filepath: &str, globals: &GLOBALS) -> String {
     println!("(...)");
 
     let frequency = target_sample_rate as f32;
-    
+
+    let filtered_signal = low_pass_filter(&resampled_samples, 5000.0, frequency);
+
     println!("Demodulating...");
-    let am_signal = envelope_detection(&resampled_samples);
+    let am_signal = envelope_detection(&filtered_signal);
+
+    let mut synced_signal = am_signal.clone();
+    // APT Signal sync
+    if *sync {
+        println!("Syncing...");
+        let frame_width = (frequency * 0.5) as usize;
+        synced_signal = sync_apt(&am_signal, frame_width);
+    }
 
     // **************
     // TODO: Delete this later
@@ -99,36 +107,85 @@ pub fn compute_signal(filepath: &str, globals: &GLOBALS) -> String {
     // let am_envelope: Vec<f32> = am_signal.iter().map(|x| x.norm() as f32).collect();
     // let synced_signal = sync(&am_signal);
 
-    let path = generate_image(&am_signal, frequency);
+    let path = generate_image(&synced_signal, frequency);
 
     return path;
 }
 
-fn sync(signal: &Vec<f32>) -> Vec<f32> {
-    // . = -1, W = 1
-    let sample_sync_frame: [f32; 36] = [-1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0];
-
-    let mut plot = Plot::new();
-
-    let mut sync_signal = Vec::new();
-    for i in 0..(signal.len() - sample_sync_frame.len() + 1) {
-        let mut sum = 0.0;
-        for j in 0..sample_sync_frame.len() {
-            sum += signal[i + j] * sample_sync_frame[j];
+fn resample_signal(samples: Vec<f32>, ratio: f64) -> Vec<f32> {
+    let mut resampled_samples: Vec<f32> = Vec::new();
+    for i in 0..(samples.len() as f64 * ratio) as usize {
+        let index = (i as f64 / ratio) as usize;
+        if index < samples.len() {
+            let x = (i as f64 / ratio) as f64 - index as f64;
+            let y: f32 = samples[index] as f32
+                + x as f32 * ((samples[index + 1] as f32) - (samples[index] as f32));
+            resampled_samples.push(y);
         }
-        sync_signal.push(sum);
     }
-
-    let trace = Scatter::new(
-        (0..10000).collect::<Vec<usize>>(),
-        sync_signal.iter().take(10000).map(|x| *x as f64).collect::<Vec<f64>>(),
-    );
-    plot.add_trace(trace);
-    plot.write_html("plot.html");
-
-    sync_signal
+    resampled_samples
 }
 
+fn low_pass_filter(samples: &Vec<f32>, cutoff_freq: f32, sample_rate: f32) -> Vec<f32> {
+    let mut filtered_samples = Vec::with_capacity(samples.len());
+    let rc = 1.0 / (cutoff_freq * 2.0 * std::f32::consts::PI);
+    let dt = 1.0 / sample_rate;
+    let alpha = dt / (rc + dt);
+    let mut prev = samples[0];
+
+    for &sample in samples.iter() {
+        let filtered = prev + alpha * (sample - prev);
+        filtered_samples.push(filtered);
+        prev = filtered;
+    }
+
+    filtered_samples
+}
+
+fn normalize_image(image: &mut GrayImage) {
+    let max_value = *image.iter().max().unwrap();
+    let min_value = *image.iter().min().unwrap();
+
+    for pixel in image.iter_mut() {
+        *pixel = (((*pixel as f32 - min_value as f32) / (max_value as f32 - min_value as f32))
+            * 255.0) as u8;
+    }
+}
+
+fn sync_apt(signal: &Vec<f32>, frame_width: usize) -> Vec<f32> {
+    let mut synced_signal = Vec::with_capacity(signal.len());
+    let mut line_start = 0;
+    let search_width = 50; // Szerokość okna poszukiwania impulsu sync
+    
+    while line_start + frame_width <= signal.len() {
+        // Szukaj impulsu sync w małym oknie na początku linii
+        let mut max_val = f32::MIN;
+        let mut max_pos = 0;
+        let search_end = (line_start + search_width).min(signal.len());
+        
+        for i in line_start..search_end {
+            if signal[i] > max_val {
+                max_val = signal[i];
+                max_pos = i;
+            }
+        }
+        
+        // Oblicz offset dla tej linii
+        let offset = max_pos - line_start;
+        
+        // Skopiuj linię z offsetem
+        for i in 0..frame_width {
+            let src_pos = line_start + ((i + offset) % frame_width);
+            if src_pos < signal.len() {
+                synced_signal.push(signal[src_pos]);
+            }
+        }
+        
+        line_start += frame_width;
+    }
+    
+    synced_signal
+}
 
 // **************
 // TODO: Delete this later
@@ -210,7 +267,7 @@ fn generate_image(signal: &Vec<f32>, frequency: f32) -> String {
             px = 0;
             py += 1;
         }
-        
+
         if py >= h {
             break;
         }
@@ -219,7 +276,7 @@ fn generate_image(signal: &Vec<f32>, frequency: f32) -> String {
     // Reduce image width by 5
     let new_w = w / 5;
     let mut img_resized: GrayImage = ImageBuffer::new(new_w, h);
-    
+
     for px in 0..new_w {
         for py in 0..h {
             let orig_px = px * 5;
@@ -227,6 +284,8 @@ fn generate_image(signal: &Vec<f32>, frequency: f32) -> String {
         }
     }
     img = img_resized;
+
+    normalize_image(&mut img);
 
     img.save("image.png").unwrap();
 
